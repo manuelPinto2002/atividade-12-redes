3eaa4a65cb8e47f93ca2e90aa45d43bf9fd250d4
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-06 11:13:18 +0000

    Update ex3.php

--- ex3.php
+++ ex3.php
@@ -9,9 +9,8 @@ $porcrapas=($npas/($npas+$nri)*100);
 $porcraparig=($nri/($npas+$nri)*100);
 
 	}
-	$a=raprap(1,2);
-$npas=rand(1,50);
-$nri=rand(1,50);
+	$a=raprap(rand(1,50),rand(1,50));
 
-	echo "n rapazes é ".$porcrapas. " e o n raparigas é de ".$porcraparig ;
+
+	echo "porcetagem rapazes é ".$porcrapas. "% e o porcentagem raparigas é de ".$porcraparig. '%' ;
  ?>
